032cca2b89b9f31443b402e0b9647911aefbf57a
Sascha <sascha.rose@gmail.com>
2023-09-15 09:29:09 +0200

    add comments

--- src/pages/ActorDetails.jsx
+++ src/pages/ActorDetails.jsx
@@ -14,13 +14,18 @@ export function ActorDetails() {
     return id == actorsId;
   });
   console.log(actor);
+
   return (
     <>
       <section>
+        {/* Was ist actorsId? Hat actorsId eine `title` Property? */}
         {actorsId.title}
         {JSON.stringify(actor)}
-       
+        {/* hat actor eine `thumbnail` Property */}
         <Link to={"/actors/" + "id"}> {actor.thumbnail}</Link>
+        {/* Sol könnte es weiter gehen: */}
+        <h3>{actor.name}</h3>
+        {/* ... */}
       </section>
     </>
   );
